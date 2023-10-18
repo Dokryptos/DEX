752dc2e729f98d37b52826967e9f5a9eee0c6936
Pierre Dok <pierredok@outlook.fr>
2023-10-18 16:53:44 +0200

    create Routes and Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-
+import { Routes, Route } from 'react-router-dom'
 import './App.css';
-
+import { Navbar } from './composents/Navbar';
+import Home from "./pages/Home"
+import List from './pages/List'
+import Portfolio from './pages/Portfolio'
+import Detail from './pages/Details'
+import Connexion from './pages/Connexion'
+ 
 function App() {
   return (
     <>
-    
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/List' element={ <List /> } />
+        <Route path='/list/details' element={ <Detail /> } />
+        <Route path='/portfolio' element={ <Portfolio />} />
+        <Route path='/connexion' element={ <Connexion />} />
+      </Routes>
     </>
   );
 }
