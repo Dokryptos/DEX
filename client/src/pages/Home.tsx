0f02add468c3ef2786f3ce80058000c47edf667b
Pierre Dok <pierredok@outlook.fr>
2023-10-18 17:05:57 +0200

    test with calling API fro Home Page GeckoTerminal

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,24 @@
-import { FunctionComponent } from "react"
+import axios from "axios"
+import { FunctionComponent, useEffect } from "react"
 
 
 const Home: FunctionComponent =() => {
+
+
+    const baseUrl = 'https://api.geckoterminal.com/api/v2'
+
+    useEffect(() => {
+        axios.get(`${baseUrl}/network`)
+        .then((element) => {
+            console.log(element);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }, []);
+
+
+
     return(
         <>
         </>
