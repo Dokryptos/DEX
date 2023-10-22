e4baa4f8c061a16054b2bc129b455fceaab09f6e
Pierre Dok <pierredok@outlook.fr>
2023-10-22 23:59:08 +0200

    test accept axios

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios, { AxiosRequestConfig } from "axios"
 import { FunctionComponent, useEffect } from "react"
 
 
@@ -6,9 +6,14 @@ const Home: FunctionComponent =() => {
 
 
     const baseUrl = 'https://api.geckoterminal.com/api/v2'
-
+    const config: AxiosRequestConfig = {
+        headers: {
+        'Accept' : 'application/json;version=20230302'
+        }
+    }
     useEffect(() => {
-        axios.get(`${baseUrl}/network`)
+        
+        axios.get(`${baseUrl}/network`, config)
         .then((element) => {
             console.log(element);
         })
